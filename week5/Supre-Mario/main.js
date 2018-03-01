32e50e13e575d491bff38b72bfabbe5d1a99bffc
BIT Student <classroom@bgit.rs>
2018-03-01 18:03:00 +0100

    super mario

---
+++ week5/Supre-Mario/main.js
@@ -0,0 +1,16 @@
+var body = document.querySelector("#body");
+var mario = document.getElementsByTagName("img");
+var stop = document.querySelector('#body');
+stop.onclick = stop;
+body.addEventListener("keydown", move);
+
+/*function move(e) {
+    if (e.keyCode === 39) {
+        // document.getElementById("mario").setAttribute("src", "mario_running.gif");
+        document.getElementById("mario").src = "mario_running.gif";
+    } else if (e.keyCode !== 39) event.preventDefault();
+}*/
+
+
+
+
